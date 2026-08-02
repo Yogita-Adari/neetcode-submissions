a403f7aeabac33482f37f60611970dfad316403d
Yogita Adari <yskadari@gmail.com>
2026-08-02 17:55:03 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    
+    def encode(self, strs: List[str]) -> str:
+        s = ""
+        for word in strs:
+            s += word + "\ssd"
+        print(s)
+        return s
+
+
+    def decode(self, s: str) -> List[str]:
+        return s.split("\ssd")[:-1]
